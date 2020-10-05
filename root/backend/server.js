11f9db69082cac102744ce35eeda4b14bc751971
radotooo <radotooo@gmail.com>
2020-10-05 18:12:22 +0300

    fetch-data

--- root/backend/server.js
+++ root/backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+
 const axios = require("axios");
 const cheerio = require("cheerio");
 
 let text;
+
 axios.get("https://comptrain.co/wod/")
     .then(res => {
 
@@ -11,6 +13,7 @@ axios.get("https://comptrain.co/wod/")
         text = $(".wod-info > div>div:first-child")
 
     })
+
 
 app.get('/', function (req, res) {
     res.send(text.html())

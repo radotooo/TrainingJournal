5c94bc19d2c4245d7d42ac091f2d64abad763703
Radislav Danev <radotooo@gmail.com>
2020-10-29 17:22:11 +0200

    Calculator component added

--- root/frontend/src/components/Calculator.js
+++ root/frontend/src/components/Calculator.js
@@ -1,38 +1,23 @@
 import React from 'react'
 import styled from "styled-components";
+import Calculator_Form from './Calculator_Form';
+import device from "./Styles/MediqQueries";
+
 
 
 const Wrapper = styled.div`
 display:flex;
+flex-direction:column;
 justify-content:center;
+align-items:center;
 `;
 
-const Input = styled.input.attrs(props => ({
-    type: "text"
-}))`
-width:${props => props.size + "px"};
-
-`;
-
-const Form = styled.div`
-display: flex;
-justify-content: center;
-align-items: flex - start;
-flex-wrap: wrap;
-margin-top: 20px;
-
-`;
+const data = [["50%","of 100","Result 50"],["10 is what %","of 100","Result 10%"]]
 
 export default function Calculator() {
     return (
         <Wrapper>
-            <Form action="">
-                <span>What is</span>
-                <Input size="50" />
-                <span>% of</span>
-                <Input size="80" />
-                <span>?</span>
-            </Form>
+            {data.map(x=> <Calculator_Form text={x} />)}
         </Wrapper>
     )
 }

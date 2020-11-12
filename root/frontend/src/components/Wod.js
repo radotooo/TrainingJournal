466207664b7ee0928565cc8d392534868eb35c9b
radotooo <radotooo@gmail.com>
2020-11-12 19:50:55 +0200

    Minor improvements

--- root/frontend/src/components/Wod.js
+++ root/frontend/src/components/Wod.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import styled from "styled-components";
+
+const Container = styled.div`
+max-width:300px;
+background-color: #e6f9fc ;
+padding:1em;
+border-radius:0.5em;
+margin-top:2em;
+`;
+const Title = styled.div`
+height:2em;
+text-align:center;
+`;
+
+var today = new Date();
+
 
 export default function Wod({ data }) {
+
     return (
-        <div dangerouslySetInnerHTML={{ __html: data }}>
-        </div>
+        <Container >
+            <Title>{today.toLocaleDateString()}</Title>
+            <div dangerouslySetInnerHTML={{ __html: data }} ></div>
+        </Container>
     )
 }

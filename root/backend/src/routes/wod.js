abf6e6771ab01938f92c4fd4a230e9d1b3e695e3
radotooo <radotooo@gmail.com>
2020-11-23 17:32:07 +0200

    Added SQL database and some improvements

--- root/backend/src/routes/wod.js
+++ root/backend/src/routes/wod.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
     res.send("Hello World")
 })
 
